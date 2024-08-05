f00eecf6627322f05153b0233f1078056a8cdaea
Greg Hodgin <ghodgin12@gmail.com>
2024-08-05 13:07:14 -0400

    Updated

--- src/analyzer.py
+++ src/analyzer.py
@@ -3,21 +3,33 @@ from matplotlib.colors import LogNorm
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.model_selection import train_test_split, KFold, cross_val_score
-from sklearn.metrics import confusion_matrix, classification_report
-from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import matplotlib.pyplot as plt
 from scipy.stats import stats
 from scipy.stats import ttest_ind
 from matplotlib import colormaps
 import streamlit as st
 
+# Active jamming data
 jamming_1 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_10dbm_gaussiannoise_1.csv')
-jamming_2 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_10dbm_gaussiannoise_2.csv')
-jamming_3 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_10dbm_gaussiannoise_3.csv')
+jamming_2 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_0dbm_gaussiannoise_28.csv')
+jamming_3 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_neg10dbm_gaussiannoise_9.csv')
+
 benign_1 = pd.read_csv('active-benign-background/5ghz_activescan_background_loc1_1.csv')
-benign_2 = pd.read_csv('active-benign-background/5ghz_activescan_background_loc1_2.csv')
-benign_3 = pd.read_csv('active-benign-background/5ghz_activescan_background_loc1_3.csv')
+benign_2 = pd.read_csv('active-benign-background/5ghz_activescan_background_loc2_35.csv')
+benign_3 = pd.read_csv('active-benign-background/5ghz_activescan_floor_32.csv')
+
+# Passive jamming data
+passive_benign1 = pd.read_csv('passive-benign-background/2.4ghz_passivescan_background_loc1_2.csv')
+passive_benign2 = pd.read_csv('passive-benign-background/2.4ghz_passivescan_background_loc1_18.csv')
+passive_benign_3 = pd.read_csv('passive-benign-background/2.4ghz_passivescan_background_loc1_27.csv')
+
+passive_jamming1 = pd.read_csv('passive-gaussian-jamming/2412mhz_jamming_12dbm_gaussiannoise_1.csv')
+passive_jamming2 = pd.read_csv('passive-gaussian-jamming/2412mhz_jamming_9dbm_gaussiannoise_6.csv')
+passive_jamming3 = pd.read_csv('passive-gaussian-jamming/2412mhz_jamming_6dbm_gaussiannoise_13.csv')
 
 # Cleaning and visualizing the data
 def visualize_examples():
@@ -38,7 +50,7 @@ def visualize_examples():
 visualize_examples()
 
 # Logistic regrression model with visualizations
-def prediction_model():
+def active_prediction_model():
     
     benign = pd.concat([benign_1, benign_2, benign_3], ignore_index=True)
     jamming = pd.concat([jamming_1, jamming_2, jamming_3], ignore_index=True)
@@ -68,10 +80,48 @@ def prediction_model():
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Benign', 'Jamming'], yticklabels=['Benign', 'Jamming'])
     plt.xlabel('Predicted')
     plt.ylabel('True')
-    plt.title('Confusion Matrix')
+    plt.title('Confusion Matrix of Active Scans')
     plt.show()
 
-prediction_model()
+active_prediction_model()
+
+# Same model as above, just for passive scans
+def passive_prediction_model():
+
+
+    passive_benign = pd.concat([passive_benign1, passive_benign2, passive_benign_3], ignore_index=True)
+    passive_jamming = pd.concat([passive_jamming1, passive_jamming2, passive_jamming3], ignore_index=True)
+
+    # Set binary values to benign and jamming
+    passive_benign['label'] = 0
+    passive_jamming['label'] = 1
+
+    # Combine the data
+    combined_passive = pd.concat([passive_benign, passive_jamming])
+
+    passive_X = combined_passive.drop(['label'], axis=1)
+    passive_y = combined_passive['label']
+
+    # Training the model
+    X_train_passive, X_test_passive, y_train_passive, y_test_passive = train_test_split(passive_X, passive_y, test_size=0.3, random_state=42)
+
+    passive_model = LogisticRegression(max_iter=2000, random_state=42)
+    passive_model.fit(X_train_passive, y_train_passive)
+
+    passive_y_pred = passive_model.predict(X_test_passive)
+
+    # Confusion matrix
+    cm = confusion_matrix(y_test_passive, passive_y_pred)
+
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Benign', 'Jamming'], yticklabels=['Benign', 'Jamming'])
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix of Passive Scans')
+    plt.show()
+
+passive_prediction_model()
+
 
 
 
