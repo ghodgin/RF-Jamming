03c90773d60db1fa3aa62820315ee3a7f4392c20
Gregory Hodgin <gregoryhodgin@Gregorys-MBP.hsd1.va.comcast.net>
2024-08-07 10:37:20 -0400

    Updated

--- src/analyzer.py
+++ src/analyzer.py
@@ -12,10 +12,48 @@ from matplotlib import colormaps
 import streamlit as st
 
 def streamlit_app():
-    st.title('RF Spectrum Analyzer')
-    st.write("Hello streamlit")
+
+    st.sidebar.title("Table of contents")
+
+    st.title('RF Jamming Predictions')
+    st.write("By Greg Hodgin")
+    st.markdown('![rf signals gif](https://www.compasseur.com/wp-content/uploads/2023/06/3_livecheck.gif)')
+    st.write('## Overview')
+    st.write('The RF spectrum plays a pivotal role in modern communications, and with the increasing prevalence of malicious RF jamming attacks, the reliability and security of wireless systems are under significant threat. To safeguard your critical frequencies, it is essential to accurately detect and identify any disruptions, whether caused by deliberate interference or other factors. This requires robust monitoring and analysis to ensure that your communications remain secure and uninterrupted.')
+    st.write('## Hypothesis')
+    st.write('### Null Hypothesis')
+    st.write('- Active scan jamming techniques are as difficult to identify and classify as passive scan techniques.')
+    st.write('### Alternate Hypothesis')
+    st.write('- Active scan jamming techniques are easier to identify and classify than passive scan techniques.')
+    st.write('## The Data')
+    st.write('- The data was presented in a 14.5 GB zip file, with 96k spectral scans. (5k active scans, 91k passive scans)')
+    st.write('- Format: .csv')
+    st.write('- Captures instances of RF jamming (malicious) and non-jamming (benign) scenarios using a Raspberry Pi4 device ')
+    st.write('- Jamming and floor data were collected in an RF chamber to ensure controlled conditions')
+    st.write('- Background data was obtained from real-world environments, with varying interference scenarios to simulate typical usage scenarios')
+    st.write('- For the sake of this project, three .csv files were selected from the different scenarios listed above. (Active benign background, active gaussian jamming, passive benign background, and passive gaussian jamming)')
+    st.write('Data Features:')
+    st.write('- freq1: Frency bin 1')
+    st.write('- noise: Noise level')
+    st.write('- total_gain_db: Total gain in dB')
+    st.write('- base_pwr_db: Base power in dB')
+    st.write('- rssi: Received signal strength indicator')
+    st.write('- relpwr_dB: Relative power in dB')
+    st.write('- avgpwr_dB: Average power in dB')
+    st.write('## Visualizations')
+    st.image('images/Figure_1.png')
+    st.image('images/Figure_2.png')
+    st.image('images/Figure_3.png')
+    st.image('images/Figure_4.png')
+
+    st.write('## Conclusion')
+    st.write('After analyzing the metrics from the prediction model, I would reject the null hypothesis. It is easier to detect Active jamming in real world conditions, compared to Passive jamming techniques. This means that if someone wanted to maliciously attack a frequency, they would be better off focusing their efforts into passive methods. This also suggests that people who are looking for disturbances on their own frequencies, that a good first step would be to look out for any passive scans on the same frequency.')
+
+    
+
 
 # Active jamming data
+
 jamming_1 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_10dbm_gaussiannoise_1.csv')
 jamming_2 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_0dbm_gaussiannoise_28.csv')
 jamming_3 = pd.read_csv('active-gaussian-jamming/5805mhz_jamming_neg10dbm_gaussiannoise_9.csv')
@@ -92,7 +130,6 @@ def active_prediction_model():
 
     return accuracy, precision, recall, f1
 
-
 # Same model as above, just for passive scans
 def passive_prediction_model():
 
@@ -134,7 +171,6 @@ def passive_prediction_model():
     f1_passive = f1_score(y_test_passive, passive_y_pred)
 
     return accuracy_passive, precision_passive, recall_passive, f1_passive
-
 
 active_metrics = active_prediction_model()
 passive_metrics = passive_prediction_model()
